a95649feb981f3d3e729166a41a95f3c8cf610ff
Simon Lam <simon.lam78@gmail.com>
2020-09-01 17:22:08 -0700

    Starter commits

---
+++ calculator.c
@@ -0,0 +1,58 @@
+/**
+ * Author:
+ * Date:
+ *
+ * This programs provides basic calculator functionality
+ * allowing a user to enter two operands and to compute
+ * various calculated values.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char **argv) {
+
+  double a, b, result;
+  int choice;
+
+  printf("Enter operand a: ");
+  scanf("%lf", &a);
+
+  printf("Enter operand b: ");
+  scanf("%lf", &b);
+
+  printf("Enter operation:\n");
+  printf("(1) Addition\n");
+  printf("(2) Subtraction\n");
+  printf("(3) Multiplication\n");
+  printf("(4) Division\n");
+  printf("(5) Minimum\n");
+  printf("(6) Power\n");
+  printf("(7) log_a(b)\n");
+  scanf("%d", &choice);
+
+  switch(choice) {
+    case 1:
+      printf("%f", a + b);
+      break;
+    case 2:
+      result = a - b;
+      printf("%f", result);
+      break;
+    case 3:
+      //TODO: handle this case
+    case 4:
+      //TODO: handle this case
+    case 5:
+      //TODO: handle this case
+    case 6:
+      //TODO: handle this case
+    case 7:
+      //TODO: handle this case
+    default:
+      printf("Please input a valid operator next time");
+  }
+
+  return 0;
+}
---
+++ leapyear.c
@@ -0,0 +1,71 @@
+/**
+ * This program determines if various years are leap
+ * years or not.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * Returns true (1) if the given year is a leap year,
+ * false (0) if it is not a leap year.
+ */
+int isLeapYear(int year);
+
+int main(int argc, char **argv) {
+
+  int year;
+  int numPassed = 0;
+  int numFailed = 0;
+
+  //Hard-coded ad-hoc test cases
+  //Do not change these, add your own test cases
+  //below.  All test cases should pass.
+
+  year = 2000;
+  printf("Test Case 1: year = %d: ", year);
+  if(!isLeapYear(year)) {
+    printf("FAILED!\n");
+    numFailed = numFailed + 1;
+  } else {
+    printf("PASSED!\n");
+    numPassed = numPassed + 1;
+  }
+
+  year = 2001;
+  printf("Test Case 2: year = %d: ", year);
+  if(isLeapYear(year)) {
+    printf("FAILED!\n");
+    numFailed = numFailed + 1;
+  } else {
+    printf("PASSED!\n");
+    numPassed = numPassed + 1;
+  }
+
+  year = 2100;
+  printf("Test Case 3: year = %d: ", year);
+  if(isLeapYear(year)) {
+    printf("FAILED!\n");
+    numFailed = numFailed + 1;
+  } else {
+    printf("PASSED!\n");
+    numPassed = numPassed + 1;
+  }
+
+  //TODO: write at least 3 more of your own test cases
+
+  printf("\n\n");
+  printf("Summary:\n");
+  printf("Number of test cases passed: %d\n", numPassed);
+  printf("Number of test cases failed: %d\n", numFailed);
+  printf("Percentage Passed: %.2f%%\n", (double) numPassed / (numPassed + numFailed) * 100.0);
+
+  return 0;
+}
+
+int isLeapYear(int year) {
+  //TODO: Write your logic here
+  //      The year is stored in the variable year
+  //      Your function should return true (1) if it represents a leap year
+  //      and false (0) if it does not.
+}
