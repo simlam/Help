5f7213a7ed77ec162f10d46e5b5af788306e2b9b
Simon Lam <simon.lam78@gmail.com>
2020-09-02 10:50:53 -0700

    This is the correct airDistance code with variations because of pi.

--- airDistance.c
+++ airDistance.c
@@ -14,45 +14,63 @@ int main(int argc, char **argv) {
   double lonDes;
   double latdesRad;
   double londesRad;
+  double sinlat;
+  double coslat;
   double airDis;
 
 printf("Origin: ");
-scanf("&lf, %lf\n", &latLoc, &lonLoc );
+scanf("%lf, %lf", &latLoc, &lonLoc);
 
-  if(lonLoc > -91 && lonLoc < 91){
-    lonlocRad = (lonLoc / 180) * 3.14;
-  }
-  else{
-  printf("fail\n");
-  }
-  if(latLoc > -181 && latLoc < 181){
-    latlocRad = (latLoc / 180) * 3.14;
-  }
-  else{
-    printf("fail\n");
-  }
+
 
 printf("Destination: ");
-scanf("%lf, %lf\n", &latDes, &lonDes );
+scanf("%lf, %lf", &latDes, &lonDes );
 
-  if(lonDes > -91 && lonDes < 91){
+//location conversion
+
+  if(lonLoc <= -91 && lonLoc >= 91){
+    printf("Fail\n");
+  }
+  else{
+    lonlocRad = (lonLoc / 180) * 3.14;
+  }
+
+  if(latLoc <= -181 && latLoc >= 181){
+    printf("Fail\n");
+  }
+  else{
+    latlocRad = (latLoc / 180) * 3.14;
+  }
+
+//destination conversion
+
+  if(lonDes <= -91 && lonDes >= 91){
+    printf("Fail\n");
+  }
+  else{
     londesRad = (lonDes / 180) * 3.14;
   }
-  else{
-    printf("fail\n");
+  if(latDes <= -181 && latDes >= 181){
+    printf("Fail\n");
   }
-  if(latDes > -181 && latDes < 181){
+  else{
     latdesRad = (latDes / 180) * 3.14;
   }
-  else{
-    printf("fail\n");
-  }
 
-airDis = acos((sin(latlocRad))*(sin(latdesRad)) + ((cos(latlocRad))*(cos(latdesRad))*(cos(londesRad - lonlocRad))) * (6371);
+
+//calculations
+
+sinlat = sin(latlocRad) * sin(latdesRad);
+coslat = cos(latlocRad) * cos(latdesRad) * cos(londesRad - lonlocRad);
+airDis = acos(sinlat + coslat) * 6371;
+
+//outputs
 
 printf("Origin: (%lf, %lf)\n", latLoc, lonLoc);
 printf("Destination: (%lf, %lf)\n", latDes, lonDes);
-printf("Air Distance is %lf\n", airDis);
+printf("Air Distance is %lf kms\n", airDis);
+
+//Margin of error based on value used for pi
 
 return 0;
 }
