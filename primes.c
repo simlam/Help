d7055ec29b2ca184e51f5cc4f15b52b5c3c8d71a
Simon Lam <simon.lam78@gmail.com>
2020-09-03 16:59:33 -0700

    Lab 4 completely done with this edit.  P.S. fuck math...

--- primes.c
+++ primes.c
@@ -11,40 +11,46 @@ int main(int argc, char **argv) {
   int n;
   int i;
   int j;
-  int isPrime = 0;    //flag variable
+  int isPrime;    //flag variable 0 = no, 1 = yes
 
 printf("Enter a number to check for primality of intergers up to that value: ");
 scanf("%d", &n);
 
 //Use nested for loop. One to print results as you iterate through n, and another to check if prime as you iterate.
 //Brain block can't figure out math for the life of me.
-  for(i=2; i<=n; i++) {
+//Figured it out ... if i is divisible by any number j leading up to i, it is not prime, therefore break.
+  for(i = 2; i <= n; i ++) {
 
-    for (j = 2; j < i; j++) {
+    for (j = 2; j <= i; j ++) {
 
-      if ( j % i == 0) {
+      if ( i % j == 0) {
+
+        isPrime = 0;
 
         break;
 
       }
-      else {
 
-        isPrime = 1;
+    }
 
-      }
+    if (i == j) {
+
+      isPrime = 1;
 
     }
 
     if(isPrime) {
+
       printf("%d is prime\n", i);
+
     }
     else {
+
       printf("%d is composite\n", i);
+
     }
 
   }
-
-
 
   return 0;
 }
