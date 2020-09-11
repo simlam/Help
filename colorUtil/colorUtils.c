08c5c9cafddc2a327cd03a812200263fae0ba4c4
Simon Lam <simon.lam78@gmail.com>
2020-09-11 09:23:16 -0700

    Made the function prototypes. may or may not work cumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcumcum

--- colorUtil/colorUtils.c
+++ colorUtil/colorUtils.c
@@ -7,7 +7,39 @@ int max(int x, int y, int z) {
   return m;
 }
 
+int min(int x, int y, int z){
+  int m = x > y ? y : x;
+  m = m > z ? z : m;
+  return m;
+}
+
 int toGrayScaleAverage(int r, int g, int b) {
   //TODO: test this, it may be wrong!
   return (r + g + b) / 3;
+}
+
+int toGrayScaleLightness(int r, int g, int b){
+  int max = r >  g ? r : g;
+  max = max > b ? max : b;
+  int min = r > g ? g : r;
+  min = min > b ? b : min;
+
+  return (max + min) / 2;
+
+}
+
+int toGrayScaleLuminosity(int r, int g, int b){
+  return 0.21(r) + 0.72(g) + 0.07(b);
+}
+
+int toSepiaRed(int r, int g, int b){
+  return 0.393(r) + 0.769(g) + 0.189(b);
+}
+
+int toSepiaBlue(int r, int g, int b){
+  return 0.349(r) + 0.686(g) + 0.168(b);
+}
+
+int toSepiaGreen(int r, int g, int b){
+  return 0.272(r) + 0.534(g) + 0.131(b);
 }
--- colorUtil/colorUtils.h
+++ colorUtil/colorUtils.h
@@ -5,11 +5,33 @@
  * Returns the maximum value among the three
  * given integer values.
  */
+
+//This is the lightness technique
+
 int max(int x, int y, int z);
+
+//Return the minimum value among the three given interger values
+
+int min(int x, int y, int z);
+
+int toGrayScaleLightness(int r, int g, int b);
 
 /**
  * Returns a gray-scaled RGB value of the three
  * given RGB values using the average technique.
  *
  */
+
 int toGrayScaleAverage(int r, int g, int b);
+
+//This is the luminosity technique
+
+int toGrayScaleLuminosity(int r, int g, int b);
+
+//Sepias
+
+int toSepiaRed(int r, int g, int b);
+
+int toSepiaGreen(int r, int g, int b);
+
+int toSepiaBlue(int r, int g, int b);
