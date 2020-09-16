d0fb18420f4eb6d42c23c1f11cd0570e204e2708
Simon Lam <simon.lam78@gmail.com>
2020-09-15 19:12:45 -0700

    bad parenthesis use im dum dum

--- colorUtil/colorUtils.c
+++ colorUtil/colorUtils.c
@@ -32,17 +32,17 @@ int toGrayScaleLightness(int r, int g, int b){
 }
 
 int toGrayScaleLuminosity(int r, int g, int b){
-  return 0.21(r) + 0.72(g) + 0.07(b);
+  return (0.21 * r) + (0.72 * g) + (0.07 * b);
 }
 
 int toSepiaRed(int r, int g, int b){
-  return 0.393(r) + 0.769(g) + 0.189(b);
+  return (0.393 * r) + (0.769 * g) + (0.189 * b);
 }
 
 int toSepiaBlue(int r, int g, int b){
-  return 0.349(r) + 0.686(g) + 0.168(b);
+  return (0.349 * r) + (0.686 * g) + (0.168 * b);
 }
 
 int toSepiaGreen(int r, int g, int b){
-  return 0.272(r) + 0.534(g) + 0.131(b);
+  return (0.272 * r) + (0.534 * g) + (0.131 * b);
 }
