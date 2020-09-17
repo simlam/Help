e0b4dd3ae61a1e85cf86669b535f505eaab6525a
Simon Lam <simon.lam78@gmail.com>
2020-09-16 22:53:50 -0700

    added the new hack and started basic structure

---
+++ Hack5/airDistance.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "airDistance.h"
+
+double degreesToRadians(double degree) {
+
+	double radians = degree * 3.14;
+	return radians / 180;
+
+}
---
+++ Hack5/airDistance.h
@@ -0,0 +1,9 @@
+//This header file designates the functions we will be designing and using
+
+//This is the function to convert degrees to radians
+
+double degreesToRadians(double degree);
+
+//This is the function that gets the distance between two locations identified by longitude/latitude
+
+double getAirDistance(double originLatitude, double originLongitude, double destinationLatitude, double destinationLongitude);
