03512f2ad1698bcdb7c8f37cb6c4ad01c9f38d81
Simon Lam <simon.lam78@gmail.com>
2020-09-17 19:19:44 -0700

    Fixed the pointers and references.  Possibly ready to go through testing but I don't have cmocka to compile.

--- Lab6/colorUtils.c
+++ Lab6/colorUtils.c
@@ -1,6 +1,19 @@
 #include <stdlib.h>
 #include <math.h>
 
+
+#ifndef MODE_H
+#define MODE_H
+
+typedef enum {
+	AVERAGE,
+	LIGHTNESS,
+	LUMINOSITY
+} Mode;
+
+#endif // !MODE_H
+
+
 #include "colorUtils.h"
 
 int max(int a, int b, int c) {
@@ -13,42 +26,47 @@ int min(int a, int b, int c) {
 
 int toGrayScale(int* r, int* g, int* b, Mode mode) {
 	//Actually don't know if this is the way it's supposed to be 
-	//Pointer to store the results of all calculations and pass through
 
-	int* result;
+	int rd;
+	int gn;
+	int bl;
 	
+	r = &rd;
+	g = &gn;
+	b = &bl;
+
 	//One will be Average
 
-	int avg = (r + g + b) / 3;
+	int avg = (rd + gn + bl) / 3;
 
 	//One will be Lightness
 
-	int max = r > g ? r : g;
-	max = max > b ? max : b;
-	int min = r > g ? g : r;
-	min = min > b ? b : min;
+	int max = rd > gn ? rd : gn;
+	max = max > bl ? max : bl;
+	int min = rd > gn ? gn : rd;
+	min = min > bl ? bl : min;
 	int light = (max + min) / 2;
 
 	//One will be Luminosity
 
-	int lum = (0.21 * r) + (0.72 * g) + (0.07 * b);
+	int lum = (0.21 * rd) + (0.72 * gn) + (0.07 * bl);
 
-	if (Mode = 0)
+	if (mode == 0)
 	{
-		result = &avg;
-		return result;
+		r = g = b = &avg;
+		return 0;
 	}
 
-	else if (Mode = 1)
+	else if (mode == 1)
 	{
-		result = &light;
-		return result;
+		r = g = b = &light;
+		return 0;
 	}
 
-	else if (Mode = 2)
+	else if (mode == 2)
 	{
-		result = &lum;
-		return result;
+		r = g = b = &lum;
+		return 0;
 	}
 
 	else {
@@ -58,21 +76,31 @@ int toGrayScale(int* r, int* g, int* b, Mode mode) {
 }
 
 int toSepia(int* r, int* g, int* b) {
-	
-	//Pointer to hold results 
 
-	int* results;
+	int rd;
+	int gn;
+	int bl;
+
+	r = &rd;
+	g = &gn;
+	b = &bl;
 
 	//One will be Sepia Red 
 
-	int red = (0.393 * r) + (0.769 * g) + (0.189 * b);
+	int red = (0.393 * rd) + (0.769 * gn) + (0.189 * bl);
 
 	//One will be Sepia Green
 
-	int green = (0.272 * r) + (0.534 * g) + (0.131 * b);
+	int green = (0.272 * rd) + (0.534 * gn) + (0.131 * bl);
 
 	//One will be Sepia Blue
 
-	int blue = (0.349 * r) + (0.686 * g) + (0.168 * b);
+	int blue = (0.349 * rd) + (0.686 * gn) + (0.168 * bl);
+
+	r = &red;
+	g = &green;
+	b = &blue;
+
+	return 0;
 
 }
