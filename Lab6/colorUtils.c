affcdf8e936cff96adc2a69bf83ef793ce65326e
Simon Lam <simon.lam78@gmail.com>
2020-09-21 19:18:40 -0700

    Added a revised rounding line to get rid of the possible errors

--- Lab6/colorUtils.c
+++ Lab6/colorUtils.c
@@ -25,35 +25,38 @@ int toGrayScale(int* r, int* g, int* b, Mode mode) {
 	//One will be Average
 
 	int avg = (*r + *g + *b) / 3;
+	int avg_rd = round(avg);
 
 	//One will be Lightness
 
 	int maxC = max(*r, *g, *b);
 	int minC = min(*r, *g, *b);
 	int light = (maxC + minC) / 2;
+	int light_rd = round(light);
 
 	//One will be Luminosity
 
 	int lum = (0.21 * *r) + (0.72 * *g) + (0.07 * *b);
+	int lum_rd = round(lum);
 
 	//Also no idea if this syntax works but it should if I'm not crazy 
 	//Round to get rid of possible errors in the unit test?
 
 	if (mode == AVERAGE)
 	{
-		r = g = b = &avg;
+		r = g = b = &avg_rd;
 		return 0;
 	}
 
 	else if (mode == LIGHTNESS)
 	{
-		r = g = b = &light;
+		r = g = b = &light_rd;
 		return 0;
 	}
 
 	else if (mode == LUMINOSITY)
 	{
-		r = g = b = &lum;
+		r = g = b = &lum_rd;
 		return 0;
 	}
 
@@ -75,22 +78,25 @@ int toSepia(int* r, int* g, int* b) {
 	//One will be Sepia Red 
 
 	int red = (0.393 * *r) + (0.769 * *g) + (0.189 * *b);
+	int red_rd = round(red);
 
 	//One will be Sepia Green
 
 	int green = (0.349 * *r) + (0.686 * *g) + (0.168 * *b);
+	int green_rd = round(green);
 
 	//One will be Sepia Blue
 
 	int blue = (0.272 * *r) + (0.534 * *g) + (0.131 * *b);
+	int blue_rd = round(blue);
 
 	//I think I need to round somehow to correct errors since we are dealing with intergers
 	//Could I round while also adding in the value to the pointer i.e. x = &round(y);
 	//Something to test at a later time 
 
-	r = &red;
-	g = &green;
-	b = &blue;
+	r = &red_rd;
+	g = &green_rd;
+	b = &blue_rd;
 
 	return 0;
 
