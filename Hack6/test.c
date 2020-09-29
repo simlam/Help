8892489c84cbc710b0a87c8b01960e4e203fbe20
Simon Lam <simon.lam78@gmail.com>
2020-09-29 13:50:05 -0700

    created a switch case program to test values by hand

--- Hack6/test.c
+++ Hack6/test.c
@@ -15,35 +15,51 @@ int main() {
 		y,
 		k;
 
+	int op;
 
-/*
-	printf("Please enter R: ");
-	scanf("%d", &r);
-	printf("Please ener G: ");
-	scanf("%d", &g);
-	printf("Please enter B: ");
-	scanf("%d", &b);
+	printf("1 for rgbToCMYK \n2 for cmykToRGB\n");
+	scanf("%d", &op);
 
-	rgbtoCMYK(r, g, b, &c, &m, &y, &k);
+	switch (op)
+	{
+		case 1:
+			printf("Please enter R: ");
+			scanf("%d", &r);
+			printf("Please ener G: ");
+			scanf("%d", &g);
+			printf("Please enter B: ");
+			scanf("%d", &b);
 
-	printf("R: %d, G: %d, B: %d\n", r, g, b);
-	printf("C: %.2lf, M: %.2lf, Y: %.2lf, K: %.2lf\n", c, m, y, k);
-*/
+			rgbtoCMYK(r, g, b, &c, &m, &y, &k);
+
+			printf("R: %d, G: %d, B: %d\n", r, g, b);
+			printf("C: %.2lf, M: %.2lf, Y: %.2lf, K: %.2lf\n", c, m, y, k);
+			break;
+
+		case 2:
+			printf("Please enter C: ");
+			scanf("%lf", &c);
+			printf("Please ener M: ");
+			scanf("%lf", &m);
+			printf("Please enter Y: ");
+			scanf("%lf", &y);
+			printf("Please enter K: ");
+			scanf("%lf", &k);
+
+			cmyktoRGB(c, m, y, k, &r, &g, &b);
+
+			printf("R: %d, G: %d, B: %d\n", r, g, b);
+			printf("C: %.2lf, M: %.2lf, Y: %.2lf, K: %.2lf\n", c, m, y, k);
+	
+			break;
+
+		default:
+			printf("INVALID OP");
+			break;
+	}
 
 
-	printf("Please enter C: ");
-	scanf("%lf", &c);
-	printf("Please ener M: ");
-	scanf("%lf", &m);
-	printf("Please enter Y: ");
-	scanf("%lf", &y);
-	printf("Please enter K: ");
-	scanf("%lf", &k);
 
-	cmyktoRGB(c, m, y, k, &r, &g, &b);
-
-	printf("R: %d, G: %d, B: %d\n", r, g, b);
-	printf("C: %.2lf, M: %.2lf, Y: %.2lf, K: %.2lf\n", c, m, y, k);
 
 	return 0;
 
