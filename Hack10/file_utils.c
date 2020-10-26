033d0fa7bbdfec4a82aa03dea68c2783465274e8
Simon Lam <simon.lam78@gmail.com>
2020-10-26 12:21:48 -0700

    did it all but didnt make test driver since not required LOL

--- Hack10/file_utils.c
+++ Hack10/file_utils.c
@@ -6,6 +6,46 @@
 
 char* getFileContents(const char* filePath) {
 
+	int const size = 500;
+	char tempBuffer[size];
 
+	FILE* read = fopen(filePath, "r");
+	if (read == NULL)
+	{
+		fprintf(stderr, "Unable to open file: %s\n", filePath);
+		return NULL;
+	}
+
+	char* string = fgets(tempBuffer, size, read);
+	fclose(read);
+	return string;
+
+}
+
+char** getFileLines(const char* filePath, int* numLines) {
+
+	int const size = 500;
+	char tempBuffer[size];
+	int i = 0;
+
+	char** strings = (char**)malloc(sizeof(char*) * 500);
+
+	FILE* read = fopen(filePath, "r");
+	if (read == NULL)
+	{
+		fprintf(stderr, "Unable to open file: %s\n", filePath);
+		return NULL;
+	}
+
+	while (fgets(tempBuffer, size, read) != NULL)
+	{
+		tempBuffer[strlen(tempBuffer) - 1] = '\0';
+		char* lineToken = strtok(tempBuffer, "\n");
+		strcpy(strings[i], lineToken);
+		i++;
+	}
+
+	fclose(read);
+	return strings;
 
 }
