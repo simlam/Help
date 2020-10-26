aa2dbd7902b99498fd91096bf41904d4f68ebeb5
Simon Lam <simon.lam78@gmail.com>
2020-10-26 11:25:11 -0700

    refer to string labs to fill it out not that bad of a hack

---
+++ Hack10/file_utils.h
@@ -0,0 +1,5 @@
+char* getFileContents(const char* filePath);
+/*This will gather the contents in a file as a whole string with no cuts*/
+
+char* getFileLines(const char* filePath, int* numLines);
+/*This will create an array of strings with each line of the file as an element in the array*/
