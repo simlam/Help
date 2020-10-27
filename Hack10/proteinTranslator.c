4dcf669a00b272b9e203b6735a080e455d0411f9
Simon Lam <simon.lam78@gmail.com>
2020-10-26 19:55:14 -0700

    asdhfjkhjkasdg?

--- Hack10/proteinTranslator.c
+++ Hack10/proteinTranslator.c
@@ -3,10 +3,13 @@
 
 #include "protein_utils.h"
 
+void rtrim(char* str); 
+
 int main(int argc, char** argv) {
 
     int size = 500;
     char tempBuffer[size];
+    int i;
 
     if (argc != 3) {
         fprintf(stderr, "Usage: %s in.txt out.txt\n", argv[0]);
@@ -14,7 +17,6 @@ int main(int argc, char** argv) {
     }
 
     FILE* read = fopen(argv[1], "r");
-    fgets(tempBuffer, size, read);
     char protein = rnaToProtein(tempBuffer);
     fclose(read);
 
