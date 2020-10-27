eee136d580ecae991c79b10135a23f953ff59f68
Simon Lam <simon.lam78@gmail.com>
2020-10-26 20:45:52 -0700

    protein fix

--- Hack10/proteinTranslator.c
+++ Hack10/proteinTranslator.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "protein_utils.h"
 
-
+void rtrim(char* str);
 
 int main(int argc, char** argv) {
 
     int size = 100;
     char tempBuffer[size];
+    char* protein;
 
     if (argc != 3) {
         fprintf(stderr, "Usage: %s in.txt out.txt\n", argv[0]);
@@ -18,7 +20,8 @@ int main(int argc, char** argv) {
 
     FILE* read = fopen(argv[1], "r");
     fgets(tempBuffer, size, read);
-    char protein = rnaToProtein(tempBuffer);
+    *protein = rnaToProtein(tempBuffer);
+    rtrim(protein);
     fclose(read);
 
     FILE* translate = fopen(argv[2], "w");
@@ -27,4 +30,13 @@ int main(int argc, char** argv) {
     
     return 0;
 
+}
+
+void rtrim(char* str) {
+    int n = strlen(str);
+    while (n >= 0 && isspace(str[n - 1])) {
+        str[n - 1] = '\0';
+        n--;
+    }
+    return;
 }
