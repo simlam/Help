6bcb1bc96bae75d1a9f9e4a551d234038e720104
Simon Lam <simon.lam78@gmail.com>
2020-10-26 21:24:02 -0700

    skajdhfhjkds

--- Hack10/proteinTranslator.c
+++ Hack10/proteinTranslator.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 #include "protein_utils.h"
 
@@ -11,7 +10,8 @@ int main(int argc, char** argv) {
 
     int size = 100;
     char tempBuffer[size];
-    char* protein;
+    int i = 0;
+    char codon[size];
 
     if (argc != 3) {
         fprintf(stderr, "Usage: %s in.txt out.txt\n", argv[0]);
@@ -19,23 +19,23 @@ int main(int argc, char** argv) {
     }
 
     FILE* read = fopen(argv[1], "r");
-    fgets(tempBuffer, size, read);
-    protein = rnaToProtein(tempBuffer);
-    rtrim(protein);
+
+    i = 0;
+    while (fgets(tempBuffer, size, read) != NULL)
+    {
+        char* rna = strtok(tempBuffer, " ");
+        codon[i] = rnaToProtein(rna);
+        i++;
+    }
     fclose(read);
+
     FILE* translate = fopen(argv[2], "w");
-    fprintf(translate, "%s", protein);
+    for (i = 0; i < size; i++)
+    {
+        fprintf(translate, "%c", codon[i]);
+    }
     fclose(translate);
     
     return 0;
 
-}
-
-void rtrim(char* str) {
-    int n = strlen(str);
-    while (n >= 0 && isspace(str[n - 1])) {
-        str[n - 1] = '\0';
-        n--;
-    }
-    return;
 }
